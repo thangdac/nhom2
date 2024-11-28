9b3355797aef200bc545bf3f84923824d22cf6c3
dovan <dovantien24112003@gmail.com>
2024-11-28 16:16:39 +0700

    backgroud

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,5 @@
-// lib/screens/login_screen.dart
-
 import 'package:flutter/material.dart';
-import 'package:nhom2/utils/auth.dart';  // Import the auth.dart file
-import 'main_screen.dart';  // Import the main screen to navigate after successful login
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -25,59 +22,164 @@ class _LoginScreenState extends State<LoginScreen> {
       _isLoading = true;  // Start loading
     });
 
-    bool success = await Auth.login(username, password);
+    // Simulate login
+    await Future.delayed(const Duration(seconds: 2));
 
     setState(() {
       _isLoading = false;  // Stop loading
     });
 
-    if (success) {
-      // If login is successful, navigate to the main screen
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const MainScreen()),
-      );
-    } else {
-      // If login failed, show an error message
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Invalid username or password')),
-      );
-    }
+    // Normally you would authenticate the user here
+    // For now, just a simple success message
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Login Successful!')),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Adding the logo
-            Image.asset(
-              'assets/facebook_logo.png', // Replace with the actual path to your logo image
-              height: 100,
+      body: Stack(
+        children: [
+          // Background image
+          Positioned.fill(
+            child: Image.asset(
+              'assets/backround2_logo.png', // Replace with your background image path
+              fit: BoxFit.cover,
             ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
+          ),
+          Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 35.0, vertical: 50.0),
+                child: Container(
+                  padding: const EdgeInsets.all(30.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.7), // Opacity to make the background transparent
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        blurRadius: 10.0,
+                        spreadRadius: 2.0,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // Title
+                      Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      // Username input
+                      TextField(
+                        controller: _usernameController,
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          prefixIcon: Icon(Icons.person),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      // Password input
+                      TextField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          prefixIcon: Icon(Icons.lock),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      // Forgot password
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {
+                            // Handle forgot password
+                          },
+                          child: const Text('Forgot password?'),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      // Login button
+                      _isLoading
+                          ? CircularProgressIndicator()
+                          : ElevatedButton(
+                        onPressed: _handleLogin,
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 15.0),
+                          minimumSize: Size(double.infinity, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          backgroundColor: Colors.blueAccent,
+                        ),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      // Social login options
+                      const Text('Or Sign Up Using'),
+                      const SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: const Icon(FontAwesomeIcons.facebook),
+                            onPressed: () {
+                              // Handle Facebook login
+                            },
+                          ),
+                          IconButton(
+                            icon: const Icon(FontAwesomeIcons.twitter),
+                            onPressed: () {
+                              // Handle Twitter login
+                            },
+                          ),
+                          IconButton(
+                            icon: const Icon(FontAwesomeIcons.google),
+                            onPressed: () {
+                              // Handle Google login
+                            },
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      // Sign-up prompt
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('Don\'t have an account?'),
+                          TextButton(
+                            onPressed: () {
+                              // Navigate to Sign Up screen
+                            },
+                            child: const Text('Sign Up'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 20),
-            _isLoading
-                ? const CircularProgressIndicator()
-                : ElevatedButton(
-              onPressed: _handleLogin,
-              child: const Text('Log In'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
